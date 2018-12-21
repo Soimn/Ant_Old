a0082e30ee6aa4721fa0953f142e21e9fd13ba41
soimn <simon.doksrod@outlook.com>
2018-12-21 19:46:23 +0100

    Fixed mkdir

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@ SET build_dir=build
 SET install_dir=%build_dir%/bin/
 SET config=Debug
 
+md %build_dir%
 cd %build_dir%
 cmake ../cmake/ -DCMAKE_INSTALL_PREFIX=%install_dir% -DCMAKE_BUILD_TYPE=%config%
 cmake --build . --target install
