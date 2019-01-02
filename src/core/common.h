41a195b98b4eb5e67119fefafdf11c22881a1f56
Soimn <35193341+Soimn@users.noreply.github.com>
2019-01-02 15:51:25 +0100

    Update common.h

--- src/core/common.h
+++ src/core/common.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "dll_definitions.h"
-
 // Standard Library Headers
 #include <cstdlib>
 #include <cstddef>
@@ -11,39 +9,6 @@
 // GLEW & GLFW
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
-// EASTL
-// #include <EASTL/algorithm.h>
-// #include <EASTL/sort.h>
-// 
-// #include <EASTL/vector.h>
-// #include <EASTL/bitvector.h>
-// #include <EASTL/fixed_vector.h>
-// #include <EASTL/array.h>
-// #include <EASTL/tuple.h>
-// #include <EASTL/hash_map.h>
-// #include <EASTL/fixed_hash_map.h>
-// #include <EASTL/map.h>
-// #include <EASTL/fixed_map.h>
-// #include <EASTL/vector_map.h>
-// #include <EASTL/unordered_map.h>
-// 
-// #include <EASTL/string.h>
-// #include <EASTL/fixed_string.h>
-// 
-// #include <EASTL/chrono.h>
-// #include <EASTL/numeric.h>
-// #include <EASTL/numeric_limits.h>
-// #include <EASTL/optional.h>
-// #include <EASTL/type_traits.h>
-// #include <EASTL/utility.h>
-// #include <EASTL/variant.h>
-
-// TEMPORARY(Soimn): temporary implementation of operator new[] required by EASTL
-// [[maybe_unused]]
-// void* operator new[] (size_t size, [[maybe_unused]] const char* name, [[maybe_unused]] int flags, [[maybe_unused]] unsigned int debugFlags, [[maybe_unused]] const char* file, [[maybe_unused]] int line) {
-// 	return malloc(size);
-// }
 
 // BLAZE
 // #define BLAZE_BLAS_MODE 1
@@ -56,5 +21,6 @@
 // 
 // #include <blaze/Blaze.h>
 
-
+#include "dll_definitions.h"
+#include "compiler_definitions.h"
 #include "core/fixed_int.h"
